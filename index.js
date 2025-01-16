01560abdff5691b4f913345ae75f7bbf83de2afb
Imranahmed935 <imrantahir9918@gmail.com>
2025-01-16 22:16:31 +0600

    upadeuser api created

--- index.js
+++ index.js
@@ -24,20 +24,85 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
-    app.get("/bookParcel/:email", async (req, res) => {
+    app.get("/myProfile/:email", async (req, res) => {
       const email = req.params.email;
-      const query = { email:email };
-      const result = await parcelCollection.find(query).toArray()
-      res.send(result)
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
     });
 
+    app.get("/updateUser/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+
+    app.patch('/updateUser/:email', async(req,res)=>{
+      const data = req.body;
+      const email = req.params.email;
+      const filter = {email:email}
+      const update = {
+        $set:{
+          name:data.name,
+          email:data.email
+
+        }
+      }
+      const result = await userCollection.updateOne(filter, update)
+      res.send(result)
+    })
+
+    
+    app.put("/users/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const { image } = req.body;
+        if (!image) {
+          return res.status(400).json({ error: "Image URL is required" });
+        }
+        const filter = { email };
+        const updateImage = {
+          $set: {
+            photo: image,
+          },
+        };
+
+        const result = await userCollection.updateOne(filter, updateImage);
+
+        if (result.matchedCount === 0) {
+          return res.status(404).json({ error: "User not found" });
+        }
+
+        if (result.modifiedCount === 0) {
+          return res
+            .status(400)
+            .json({ error: "Image not updated. Try again." });
+        }
+
+        res.json({
+          success: true,
+          message: "Profile image updated successfully",
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.get("/bookParcel/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await parcelCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/bookParcel", async (req, res) => {
       const data = req.body;
