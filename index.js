8f2eafdfccb168e1808d6e7b7aa433a0cf857e25
Imranahmed935 <imrantahir9918@gmail.com>
2025-01-16 12:44:29 +0600

    bookParcel api added

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-
-app.use(cors())
-app.use(express.json())
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.haqk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,10 +13,30 @@ const client = new MongoClient(uri, {
   }
 });
 
+app.use(cors())
+app.use(express.json())
+
+const userCollection =client.db('parcel').collection('users');
+const parcelCollection =client.db('parcel').collection('bookParcel');
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+     app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+     })
+
+     app.post('/bookParcel', async (req, res)=>{
+      const data = req.body;
+      const result= await parcelCollection.insertOne(data);
+      res.send(result);
+     })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
