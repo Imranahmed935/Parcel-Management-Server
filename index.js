85821c05207e4b4861407d0d7d4d4726037bc561
Imranahmed935 <imrantahir9918@gmail.com>
2025-01-18 19:03:02 +0600

    verifyAdmin midleware created

--- index.js
+++ index.js
@@ -22,19 +22,31 @@ const parcelCollection = client.db("parcel").collection("bookParcel");
 const assignDeliveryMan = client.db("parcel").collection("delivery");
 
 const verifyToken = (req, res, next) => {
-  console.log('helllo man',req.headers.authorization)
   if (!req.headers.authorization) {
-    return res.status(401).send({ message: "forbidden access" });
+    return res.status(401).send({ message: "unauthorized access" });
   }
   const token = req.headers.authorization.split(" ")[1];
   jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: "forbidden access" });
+      return res.status(401).send({ message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
   });
 };
+
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === "admin";
+  if (!isAdmin) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  next();
+};
+
 
 async function run() {
   try {
@@ -60,7 +72,22 @@ async function run() {
       res.send({ token });
     });
 
-    app.get("/users", verifyToken, async (req, res) => {
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+
+      res.send({ admin });
+    });
+
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
