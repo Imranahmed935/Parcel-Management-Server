99a6a94c48cb0965d9afb600cf12d47e650d1f8c
Imranahmed935 <imrantahir9918@gmail.com>
2025-01-18 17:58:33 +0600

    jwt added for security

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const app = express();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.haqk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -18,7 +19,22 @@ app.use(express.json());
 
 const userCollection = client.db("parcel").collection("users");
 const parcelCollection = client.db("parcel").collection("bookParcel");
-const assignDeliveryMan = client.db('parcel').collection('delivery')
+const assignDeliveryMan = client.db("parcel").collection("delivery");
+
+const verifyToken = (req, res, next) => {
+  console.log('helllo man',req.headers.authorization)
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "forbidden access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -36,62 +52,65 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users/:role", async (req, res) => {
-      const role = req.params.role;
-      const query = { role: role };
-      const data = await userCollection.find(query).toArray();
-      res.send(data);
-    }); 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
-    app.patch('/users/:id', async (req,res)=>{
-      const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const updateDoc = {
-        $set:{
-          role:'deliveryMan'
-        }
-      }
-      const result = await userCollection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
-
-
-    app.patch('/users/admin/:id', async (req, res)=>{
-      const id  = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const updateDoc = {
-        $set:{
-          role:'admin'
-        }
-      }
-      const result = await userCollection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
-
-    app.post('/parcels/:id', async (req, res)=>{
-      const data = req.body;
-      const result = await assignDeliveryMan.insertOne(data)
-      res.send(result)
-    })
-
-    app.patch('/users/status/:id', async (req, res)=>{
-      const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const update = {
-        $set:{
-          status:'On the way'
-        }
-      }
-      const result = await parcelCollection.updateOne(filter, update)
-      res.send(result)
-
-    })
-
+    app.get("/users", verifyToken, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
     app.get("/users/:role", async (req, res) => {
       const role = req.params.role;
       const query = { role: role };
       const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "deliveryMan",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.post("/parcels/:id", async (req, res) => {
+      const data = req.body;
+      const result = await assignDeliveryMan.insertOne(data);
+      res.send(result);
+    });
+
+    app.patch("/users/status/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          status: "On the way",
+        },
+      };
+      const result = await parcelCollection.updateOne(filter, update);
       res.send(result);
     });
 
@@ -121,6 +140,7 @@ async function run() {
       const update = {
         $set: {
           name: data.name,
+          phone: data.phone,
           email: data.email,
         },
       };
